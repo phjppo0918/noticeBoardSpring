a950043ee0424c4f338e8431aed783816320b5d8
Hyeonjun Park <phjppo0918@kpu.ac.kr>
2020-03-12 20:10:27 +0900

    configuration, bean 추가

--- src/di/chap2_1_2/AppConfig.java
+++ src/di/chap2_1_2/AppConfig.java
@@ -1,6 +1,29 @@
 package di.chap2_1_2;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import di.chap2_1.BCryptPasswordEncoder;
+import di.chap2_1.PasswordEncoder;
+import di.chap2_1.UserRepository;
+import di.chap2_1.UserService;
+import di.chap2_1.UserServiceImpl;
+ 
 @Configuration
 public class AppConfig {
+	@Bean
+	UserRepository userRepository() {
+		return new UserRepositoryImpl();
+	}
+
+	@Bean
+	PasswordEncoder passwordEncoder() {
+		return new BCryptPasswordEncoder();
+	}
+	@Bean
+	UserService  userService() {
+		return new UserServiceImpl(userRepository(), passwordEncoder());
+	}
+
 
 }
---
+++ src/di/chap2_1_2/UserRepositoryImpl.java
@@ -0,0 +1,20 @@
+package di.chap2_1_2;
+
+import di.chap2_1.User;
+import di.chap2_1.UserRepository;
+
+public class UserRepositoryImpl implements UserRepository {
+
+	@Override
+	public User save(User user) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public int countByUserName(String userName) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
