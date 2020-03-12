18f6f60aa2f8179c7454c502299b3c9f1310946b
Hyeonjun Park <phjppo0918@kpu.ac.kr>
2020-03-12 21:09:06 +0900

    2.1.3. 빈 설정 진행중

--- src/di/chap2_1_2/AppConfig.java
+++ src/di/chap2_1_2/AppConfig.java
@@ -1,29 +1,40 @@
-package di.chap2_1_2;
-
+package di.chap2_1_2; 
+ 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-
+ 
 import di.chap2_1.BCryptPasswordEncoder;
 import di.chap2_1.PasswordEncoder;
 import di.chap2_1.UserRepository;
 import di.chap2_1.UserService;
 import di.chap2_1.UserServiceImpl;
  
-@Configuration
+@Configuration //설정 클래스 선언
 public class AppConfig {
-	@Bean
+	@Bean //빈 정의. 메서드 이름이 빈이 이름
 	UserRepository userRepository() {
 		return new UserRepositoryImpl();
 	}
 
-	@Bean
+	@Bean(name="PWEncoder")//빈의 이름을 다르게 명시하고 싶을 때 재정의
 	PasswordEncoder passwordEncoder() {
 		return new BCryptPasswordEncoder();
 	}
 	@Bean
 	UserService  userService() {
+		
+		return new UserServiceImpl(userRepository(), passwordEncoder());
+		//다른 컴포넌트를 참조해야 할 때는 해당 컴포넌트의 메서드를 호출
+		//의존성 주입을 프로그램적인 방법으로 처리
+	}
+	
+	//자바 기반 설정방식에서는 메서드에 매개변수를 추가하는 방법으로 다른 컴포넌트의 의존성을 주입할 수 있다.
+	//단, 인수로 전달된 인스턴스에 대한 빈은 별도로 정이돼 있어야 한다.
+
+	@Bean
+	UserService userService(UserRepository userRepository, PasswordEncoder passwordEncoder) {
+		
 		return new UserServiceImpl(userRepository(), passwordEncoder());
 	}
-
-
+	
 }
--- src/di/chap2_1_2/Application.java
+++ src/di/chap2_1_2/Application.java
@@ -1,5 +1,38 @@
 package di.chap2_1_2;
 
-public class Application {
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.context.support.FileSystemXmlApplicationContext;
 
+import di.chap2_1.UserService;
+
+public class Application {
+	public static void main(String [] args) {
+		
+		//ApplicationContext 생상하는 다양한 방법
+		
+		//자바 기반 설정방식
+		ApplicationContext context = new AnnotationConfigApplicationContext(AppConfig.class);
+		//에너테이션 기반의 설정 방식
+		ApplicationContext context1 = new AnnotationConfigApplicationContext("com.example.app");
+		//XML 기반 설정 방식. 경로에 접두어가 생략된 경우에는 클래스패스 안에서 상대 경로로 설정 파일을 탐색
+		ApplicationContext context2 =
+				new ClassPathXmlApplicationContext("META_INF/spring/applicationContext.xml");
+		//XML 기반 설정 방식. 경로에 접두어가 생략된 경우에는 JVM의 직업 디렉터리 안에서 상대 결로로 설정 파일을 탐생한다.
+		ApplicationContext context3 =
+				new FileSystemXmlApplicationContext("./spring/applicationContext.xml");
+		
+		
+		
+		//DI 컨테이너에서 빈 가져오기
+		
+		//빈의 type을 지정하는 방법. 지정한 타입에 해당하는 빈이 DI 컨테이너에 오직 하나만 있을 때 사용
+		UserService userService = context.getBean(UserService.class);
+		//가져오려는 빈의 이름과 타입을 지정하는 방법. 지정한 타입에 해당하는 빈이 DI 컨테이너에 여러 개 있을 때 사용
+		UserService userService1 = context.getBean("UserService", UserService.class);
+		//가져오려는 빈의 이름을 지정하는 방법. 캐스트 필수
+		UserService userService2 = (UserService)context.getBean("UserService");
+		
+	}
 }
